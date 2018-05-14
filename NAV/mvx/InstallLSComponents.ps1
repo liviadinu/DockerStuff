bb83db64b9564f9d9e473120a4ef23ff5e8e220c
Livia Dinu <Livia.Dinu@k3business.nl>
2018-05-14 21:05:55 +0200

    second commit

--- NAV/mvx/InstallLSComponents.ps1
+++ NAV/mvx/InstallLSComponents.ps1
@@ -3,9 +3,14 @@
 if(!$isRestarting){
 	Write-Host 'Installing LS Retail Components...'
 	Set-ExecutionPolicy Bypass -Scope Process -Force 
-	Start-Process "C:\run\mvx\LS Nav 10.10.00.555 Service Components.exe" -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /LOADINF=C:\run\mvx\lsretail.inf' -Wait -NoNewWindow
-	Start-Process "C:\run\mvx\LS Nav 10.10.00.555 Client Components.exe" -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /LOADINF=C:\run\mvx\lsretail.inf' -Wait -NoNewWindow
-
+	#Start-Process "C:\run\mvx\LS Nav 10.10.00.555 Service Components.exe" -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /LOADINF=C:\run\mvx\lsretail.inf' -Wait -NoNewWindow
+	#Start-Process "C:\run\mvx\LS Nav 10.10.00.555 Client Components.exe" -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /LOADINF=C:\run\mvx\lsretail.inf' -Wait -NoNewWindow
+ 
+	iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	choco
+	choco feature enable -n allowGlobalConfirmation
+	cinst "C:\run\mvx\lsclientcomp.10.10.nupkg"
+	cinst "C:\run\mvx\lsservicecomp.10.10.nupkg"
 	Write-Host 'Copying LS Retail .dlls to Add-ins folder...'
 
 	Copy-Item -Path "C:\run\mvx\DD" -Destination "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\Add-ins\" -Recurse
