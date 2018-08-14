819fb307a27ca487ec89c32ce001f3435109f9e7
Livia Dinu <Livia.Dinu@k3business.nl>
2018-08-14 21:20:41 +0200

    change component script for 2018

--- NAV/mvx/AdditionalMvComponents.ps1
+++ NAV/mvx/AdditionalMvComponents.ps1
@@ -1,14 +1,14 @@
 $mvxpath = "C:\run\mvx\CompInstall.txt"
 [bool]$isRestarting = [System.IO.File]::Exists($mvxpath)
 if(!$isRestarting){
-	$serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\100\Service").FullName
+	$serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\110\Service").FullName
 	if (Test-Path "$serviceTierFolder\Microsoft.Dynamics.Nav.Management.psm1") {
 		Import-Module "$serviceTierFolder\Microsoft.Dynamics.Nav.Management.psm1" -wa SilentlyContinue
 	} else {
 		Import-Module "$serviceTierFolder\Microsoft.Dynamics.Nav.Management.dll" -wa SilentlyContinue
 	}
 
-	$roleTailoredClientFolder = (Get-Item "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client").FullName
+	$roleTailoredClientFolder = (Get-Item "C:\Program Files (x86)\Microsoft Dynamics NAV\110\RoleTailored Client").FullName
 	if (Test-Path "$roleTailoredClientFolder\Microsoft.Dynamics.Nav.Apps.Management.psd1") {
 		Import-Module "$roleTailoredClientFolder\Microsoft.Dynamics.Nav.Apps.Management.psd1" -wa SilentlyContinue
 	}
